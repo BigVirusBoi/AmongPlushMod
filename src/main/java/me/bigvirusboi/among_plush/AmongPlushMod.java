d2863b26f58787972c80aaba4fad0b0920553705
BigVirusBoi <bigvirusboi@gmail.com>
2023-11-28 20:28:31 +0100

    init 2?

---
+++ src/main/java/me/bigvirusboi/among_plush/AmongPlushMod.java
@@ -0,0 +1,13 @@
+package me.bigvirusboi.among_plush;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AmongPlushMod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
